6919ae74e3a1edfe6000c067d38bc8b3aa9eff36
Jarvl <ajarvis7771@gmail.com>
2014-10-31 14:59:50 -0400

    added webhook to app.js

--- app.js
+++ app.js
@@ -150,6 +150,25 @@ var options = {
 app.use('/webhook', require('express-github-hook')(options));
 */
 
+// Listen on port 9001
+var gith = require('gith').create( 9001 );
+// Import execFile, to run our bash script
+var execFile = require('child_process').execFile;
+
+gith({
+  repo: 'Jarvl/ENG2089-Project'
+}).on( 'all', function( payload ) {
+  if( payload.branch === 'master' )
+  {
+    // Exec a shell script
+    execFile('/var/www/hook.sh', function(error, stdout, stderr) {
+      // Log success in some manner
+      console.log( 'exec complete' );
+    });
+  }
+});
+
+
 /**
  * Main routes.
  */
