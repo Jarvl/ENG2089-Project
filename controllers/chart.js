3af080e0c150b34b5b6ab368656016174c2f33a8
Jarvl <ajarvis7771@gmail.com>
2014-11-13 14:11:52 -0500

    trying display: none on citations

--- controllers/chart.js
+++ controllers/chart.js
@@ -2,10 +2,15 @@
  * Created by Andrew on 11/12/2014.
  */
 
+//var LineByLineReader = require('line-by-line')
+
 /**
  * GET /chart
  * Home page.
  */
+
+//lr = new LineByLineReader('big_file.txt');
+
 
 exports.index = function(req, res) {
     res.render('chart', {
